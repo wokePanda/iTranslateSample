781c1316e80df00de7f87559735893b507321f9e
wokePanda <florinusc2@gmail.com>
2019-06-20 12:12:14 +0300

    Add extension for UIColor

---
+++ iTranslateSample00/Helpers/Extensions/UIColor+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Extension.swift
+//  iTranslateSample00
+//
+//  Created by Florin Uscatu on 20/06/2019.
+//  Copyright © 2019 Andreas Dolinsek. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let babyBlue = UIColor(red: 26/255.0, green: 152/255.0, blue: 255/255.0, alpha: 1.0)
+}
