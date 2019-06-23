9f9033199634d480ec2e1ff44e683932c70dd30e
wokePanda <florinusc2@gmail.com>
2019-06-23 16:45:54 +0300

    Adds UI tests

--- iTranslateSample00UITests/iTranslateSample00UITests.swift
+++ iTranslateSample00UITests/iTranslateSample00UITests.swift
@@ -8,4 +8,25 @@
 
 import XCTest
 
-class iTranslateSample00UITests: XCTestCase {}
+class iTranslateSample00UITests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    
+    func test_recordingCreation_withGrantedPermission_succesful() {
+        let app = XCUIApplication()
+        app.buttons["Mic"].tap()
+        wait(for: [], timeout: 2.0)
+        app.buttons["Mic"].tap()
+        app.alerts["Success"].buttons["OK"].tap()
+    }
+    
+    func test_accessRecordingList_successful() {
+        let app = XCUIApplication()
+        app.buttons["Show Recordings"].tap()
+        app.navigationBars["Recordings"].buttons["Done"].tap()
+    }
+}
